730a3c4ea5c43199405467a44eaa0c95f9c6c712
ybenic28 <benichouyanivpro@gmail.com>
2021-01-09 02:06:01 +0100

    boucle principale prog 2 + sécurité args

--- programme1/programme1.c
+++ programme1/programme1.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-
-
 #include "../include/treatmentFiles.h"
 
 
--- programme2/programme2.c
+++ programme2/programme2.c
@@ -98,6 +98,11 @@ void writeDotGraph(const char *filename, T_graphLA *g)
   dumpGraph(fp, g);
   fprintf(fp, "}\n");
   fclose(fp);
+
+  //Affichage sortie Standard
+  char command[200];
+  snprintf(command, sizeof(command), "cat %s", file_name);
+  system(command);
 }
 
 void showGraphPNG(const char *filename, T_graphLA *g)
@@ -110,8 +115,8 @@ void showGraphPNG(const char *filename, T_graphLA *g)
   mkdir("output/png", 0777);
 
   snprintf(command, sizeof(command), "dot output/dot/%s.dot -T png -o output/png/%s.png", filename, filename);
-  printf("%s\n", command);
   system(command);
+  printf("Création du fichier : output/png/%s.png\n", filename);
 }
 
 int isNumber(char *token)
@@ -125,10 +130,25 @@ int isNumber(char *token)
   }
   return 1;
 }
-T_graphLA *matToAdj(const char *filename)
+int checkExtension(char *extensionToHave, const char *filename)
+{
+  char cp2[50];
+  strcpy(cp2, filename);
+  char *extension = strtok(cp2, ".");
+  extension = strtok(NULL, ".");
+
+  if (extension == NULL || strcmp(extension, extensionToHave) != 0)
+  {
+    printf("Verifiez l'extension du fichier ... L'argument saisi ne semble mené vers aucun/mauvais fichier ...\n");
+    exit(-1);
+  }
+  return 0;
+}
+T_graphLA *adjToGraph(const char *filename)
 {
   FILE *fp;
-  CHECK_IF(fp = fopen(filename, "r"), NULL, "fopen");
+  checkExtension("adj", filename);
+  CHECK_IF(fp = fopen(filename, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
 
   int ligne = 0, column = 0, size = 0;
   const char *separators = "\t\n";
@@ -136,14 +156,13 @@ T_graphLA *matToAdj(const char *filename)
 
   // Parcours le file pour connaitre nb_lignes = graph size
   FILE *fp_copy;
-  CHECK_IF(fp_copy = fopen(filename, "r"), NULL, "fopen");
+  CHECK_IF(fp_copy = fopen(filename, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
 
   while (fgets(line, 100, fp_copy) != NULL)
   {
     size++;
   }
   fclose(fp_copy);
-
 
   T_graphLA *g = newGraphLA(size);
 
@@ -166,10 +185,11 @@ T_graphLA *matToAdj(const char *filename)
   return g;
 }
 
-T_graphLA *laToAdj(const char *filename)
+T_graphLA *laToGraph(const char *filename)
 {
   FILE *fp;
-  CHECK_IF(fp = fopen(filename, "r"), NULL, "fopen");
+  checkExtension("la", filename);
+  CHECK_IF(fp = fopen(filename, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
 
   int ligne = 0, size = 0;
   const char *separators = "\t\n";
@@ -177,7 +197,7 @@ T_graphLA *laToAdj(const char *filename)
 
   // Parcours le file pour connaitre nb_lignes = graph size
   FILE *fp_copy;
-  CHECK_IF(fp_copy = fopen(filename, "r"), NULL, "fopen");
+  CHECK_IF(fp_copy = fopen(filename, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
 
   while (fgets(line, 100, fp_copy) != NULL)
   {
@@ -208,26 +228,33 @@ T_graphLA *laToAdj(const char *filename)
   return g;
 }
 
-void matTolist(const char *filename,const char *fichierdepart)
+void adjToLa(const char *fichierdepart)
 {
-  FILE *fp;
-  FILE *dep;
-  CHECK_IF(fp = fopen(filename, "w"), NULL, "fopen arrivée");
-  CHECK_IF(dep = fopen(fichierdepart, "r"), NULL, "fopen départ");
+  mkdir("output",0777);
+  mkdir("output/la/", 0777);
+  FILE *fp, *dep;
+  checkExtension("adj", fichierdepart);
+  CHECK_IF(dep = fopen(fichierdepart, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
+
+  char cp[50], filename[50];
+  strcpy(cp, fichierdepart);
+  snprintf(filename, sizeof(filename), "output/la/%s.la", strtok(cp + 10, "."));
+  CHECK_IF(fp = fopen(filename, "w"), NULL, "L'argument saisi ne semble pas bien être formulé ...");
+  printf("création du fichier : %s \n", filename);
 
   int ligne = 0, column = 0;
   const char *separators = "\t\n_";
   char line[100];
-  
+
   while (fgets(line, 100, dep) != NULL)
   {
-    fprintf(fp,"%d\t",ligne);
+    fprintf(fp, "%d\t", ligne);
     char *token = strtok(line, separators);
     while (token != NULL)
     {
-      if (isNumber(token) == 1 && atoi(token) != 0 && token!=0)
+      if (isNumber(token) == 1 && atoi(token) != 0 && token != 0)
       {
-        fprintf(fp,"%d_%s\t",column,token);
+        fprintf(fp, "%d_%s\t", column, token);
       }
       token = strtok(NULL, separators);
       column++;
@@ -240,18 +267,25 @@ void matTolist(const char *filename,const char *fichierdepart)
   fclose(dep);
 }
 
-void listTomat(const char *filename,const char *fichierdepart)
+void laToAdj(const char *fichierdepart)
 {
-  FILE *fp;
-  FILE *dep;
-  CHECK_IF(fp = fopen(filename, "w"), NULL, "fopen");
-  CHECK_IF(dep = fopen(fichierdepart, "r"), NULL, "fopen");
+  mkdir("output",0777);
+  mkdir("output/adj/", 0777);
+  FILE *fp, *dep;
+  checkExtension("la", fichierdepart);
+  CHECK_IF(dep = fopen(fichierdepart, "r"), NULL, "L'argument saisi ne semble mené vers aucun fichier ...");
+
+  char cp[50], filename[50];
+  strcpy(cp, fichierdepart);
+  snprintf(filename, sizeof(filename), "output/adj/%s.adj", strtok(cp + 9, "."));
+  CHECK_IF(fp = fopen(filename, "w"), NULL, "L'argument saisi ne semble pas bien être formulé ...");
+  printf("création du fichier : %s \n", filename);
 
   int ligne = 0, column = 0;
-  int size=0;
+  int size = 0;
   const char *separators = "\t\n_";
   char line[100];
-  
+
   FILE *fp_copy;
   CHECK_IF(fp_copy = fopen(fichierdepart, "r"), NULL, "fopen");
   while (fgets(line, 100, fp_copy) != NULL)
@@ -260,41 +294,123 @@ void listTomat(const char *filename,const char *fichierdepart)
   }
   fclose(fp_copy);
 
- 
   while (fgets(line, 100, dep) != NULL)
   {
     char *token = strtok(line, separators);
     token = strtok(NULL, separators);
 
-    for(column=0;column<size;column++)
-    {   
-        if (token!=NULL && atoi(token)==column)
-          {
-            token = strtok(NULL, separators); //prendre le poids associé au sommet
-            fprintf(fp,"%d\t",atoi(token));
-            token = strtok(NULL, separators);
-          }
-      
-        else if(column==ligne)
-          {
-            fprintf(fp,"0\t");
-          }
-        else
-          {
-            fprintf(fp,"i\t");
-          } 
+    for (column = 0; column < size; column++)
+    {
+      if (token != NULL && atoi(token) == column)
+      {
+        token = strtok(NULL, separators); //prendre le poids associé au sommet
+        fprintf(fp, "%d\t", atoi(token));
+        token = strtok(NULL, separators);
       }
-    fprintf(fp,"\n");
-    ligne++;
+
+      else if (column == ligne)
+      {
+        fprintf(fp, "0\t");
+      }
+      else
+      {
+        fprintf(fp, "i\t");
+      }
     }
+    fprintf(fp, "\n");
+    ligne++;
+  }
   fclose(fp);
   fclose(dep);
-}     
+}
 
-
-int main(void)
+int startsWith(const char *pre, const char *str)
 {
-  T_graphLA *g5 = newGraphLA(4);
+  size_t lenpre = strlen(pre),
+         lenstr = strlen(str);
+  return lenstr < lenpre ? 1 : memcmp(pre, str, lenpre) == 0;
+}
+
+int main(int argc, char **argv)
+{
+  T_graphLA *g;
+  if (argc == 4)
+  {
+    //on considère les chemins vers les fichiers commencant par input et non par ./input par exemple
+    // car peut causer des soucis lors de la vérification de l'extension etc...
+    if (startsWith("./", argv[3]))
+    {
+      argv[3] += 2; //suppression du ./
+    }
+
+    if (strcmp(argv[1], "-dot") == 0)
+    {
+
+      //EXEMPLE 1
+      if (strcmp(argv[2], "-a") == 0)
+      {
+        g = adjToGraph(argv[3]);
+
+        char cp[50];
+        strcpy(cp, argv[3]);
+
+        char *filename = strtok(cp + 10, ".");
+        showGraphPNG(filename, g);
+
+        // EXEMPLE 2
+      }
+      else if (strcmp(argv[2], "-l") == 0)
+      {
+        g = laToGraph(argv[3]);
+
+        char cp[50];
+        strcpy(cp, argv[3]);
+
+        char *filename = strtok(cp + 9, ".");
+        showGraphPNG(filename, g);
+      }
+      else
+      {
+        printf("\n\tL'argument : %s n'est pas connu ...\n\tVeuillez choisir un 2e argument valable parmi : -a -l\n\n", argv[2]);
+      }
+
+      // EXEMPLE 3
+    }
+    else if (strcmp(argv[1], "-la") == 0)
+    {
+      if (strcmp(argv[2], "-a") == 0)
+      {
+        adjToLa(argv[3]);
+      }
+      else
+      {
+        printf("\n\tL'argument : %s n'est pas connu ...\n\tVeuillez choisir un 2e argument valable parmi : -a\n\n", argv[2]);
+      }
+      // EXEMPLE 4
+    }
+    else if (strcmp(argv[1], "-adj") == 0)
+    {
+      if (strcmp(argv[2], "-l") == 0)
+      {
+        laToAdj(argv[3]);
+      }
+      else
+      {
+        printf("\n\tL'argument : %s n'est pas connu ...\n\tVeuillez choisir un 2e argument valable parmi : -l\n\n", argv[2]);
+      }
+    }
+    else
+    {
+      printf("\n\tL'argument : %s n'est pas connu ...\n\tVeuillez choisir un 1er argument valable parmi : -dot -la -adj\n\n", argv[1]);
+    }
+  }
+  else
+  {
+    printf("\n\t3 paramètres doivent être saisis\n\n");
+  }
+  return 0;
+
+  /*T_graphLA *g5 = newGraphLA(4);
   addEdge(g5, 0, 2, 3);
   addEdge(g5, 2, 3, 4);
   addEdge(g5, 3, 1, 1);
@@ -323,18 +439,24 @@ int main(void)
   addEdge(g4, 7, 8, 45);
   showGraphPNG("graph4", g4);
 
-  T_graphLA *g1 = matToAdj("input/adj/graph1.adj");
+  T_graphLA *g1 = adjToGraph("input/adj/graph1.adj");
   showGraphPNG("graph1", g1);
 
-  T_graphLA *g2 = matToAdj("input/adj/graph2.adj");
+  T_graphLA *g2 = adjToGraph("input/adj/graph2.adj");
   showGraphPNG("graph2", g2);
 
-  T_graphLA *g3 = matToAdj("input/adj/graph3.adj");
+  T_graphLA *g3 = adjToGraph("input/adj/graph3.adj");
   showGraphPNG("graph3", g3);
   
-  matTolist("output/graph2.la","input/adj/graph1.adj");
+  matTolist("output/graph2.la","input/adj/graph1.adj");*/
 
-  listTomat("output/graph2.adj","input/la/graph3.la");
+  /*laToAdj("input/la/graph1.la");
+  laToAdj("input/la/graph2.la");
+  laToAdj("input/la/graph3.la");
+  
+  adjToLa("input/adj/graph1.adj");
+  adjToLa("input/adj/graph2.adj");
+  adjToLa("input/adj/graph3.adj");*/
 
   return 0;
 }
